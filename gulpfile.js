a047f943670b561ba604fc368091009d35ed6e60
kiaghods <kiaghods@users.noreply.github.com>
2024-07-06 07:12:55 +0000

    deploy: 3856e23145b20a603832cc949cdd65517c421559

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ const gulp = require('gulp');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
+const sharp = require('sharp');
+const through2 = require('through2');
 
+// CSS minification task
 function minifyCSS() {
     return gulp.src('assets/css/*.css')
         .pipe(cleanCSS())
@@ -10,6 +13,7 @@ function minifyCSS() {
         .pipe(gulp.dest('assets/css'));
 }
 
+// JS minification task
 function minifyJS() {
     return gulp.src('assets/js/*.js')
         .pipe(uglify())
@@ -17,13 +21,45 @@ function minifyJS() {
         .pipe(gulp.dest('assets/js'));
 }
 
+// Image optimization task
+function optimizeImages() {
+    return gulp.src('assets/media/**/*')
+        .pipe(through2.obj(function (file, _, cb) {
+            if (file.isBuffer()) {
+                const validFormats = ['jpeg', 'png', 'webp', 'gif', 'svg'];
+                const format = sharp(file.contents).metadata().then(metadata => metadata.format);
+
+                if (validFormats.includes(format)) {
+                    sharp(file.contents)
+                        .toBuffer()
+                        .then(data => {
+                            file.contents = data;
+                            cb(null, file);
+                        })
+                        .catch(err => cb(err));
+                } else {
+                    cb(null, file);
+                }
+            } else {
+                cb(null, file);
+            }
+        }))
+        .pipe(gulp.dest('assets/media'));
+}
+
+// Watch files for changes
 function watchFiles() {
     gulp.watch('assets/css/*.css', minifyCSS);
     gulp.watch('assets/js/*.js', minifyJS);
+    gulp.watch('assets/media/**/*', optimizeImages);
 }
 
-const build = gulp.series(minifyCSS, minifyJS);
+// Build task
+const build = gulp.series(minifyCSS, minifyJS, optimizeImages);
 
-exports.default = build;
+exports.minifyCSS = minifyCSS;
+exports.minifyJS = minifyJS;
+exports.optimizeImages = optimizeImages;
 exports.watch = watchFiles;
+exports.default = build;
 exports.build = build;
